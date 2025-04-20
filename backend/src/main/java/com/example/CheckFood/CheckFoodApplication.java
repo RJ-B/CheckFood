05f401d2083a865d362db8f73bd6e32ad0b06436
RJ-B <rosta1013@gmail.com>
2025-04-20 20:55:01 +0200

    Initial commit for CheckFood project

---
+++ backend/src/main/java/com/example/CheckFood/CheckFoodApplication.java
@@ -0,0 +1,13 @@
+package com.example.CheckFood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckFoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckFoodApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/CheckFood/CheckFoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CheckFood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckFoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
